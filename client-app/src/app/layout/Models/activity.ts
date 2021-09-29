c69e409a20fff6b61b5ac9b12da625b81eaa4fb2
DirtyDancin07 <andy.swayze@gmail.com>
2021-09-28 21:32:09 -0600

    End of Section 5

---
+++ client-app/src/app/layout/Models/activity.ts
@@ -0,0 +1,11 @@
+import { StrictButtonGroupProps } from "semantic-ui-react";
+
+export interface Activity {
+  id:          string;
+  title:       string;
+  date:        string;
+  description: string;
+  category:    string;
+  city:        string;
+  venue:       string;
+}
